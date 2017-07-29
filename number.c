3027439eb1db465733296d6633f6f9865f25b0be
mythili97 <30385973+mythili97@users.noreply.github.com>
2017-07-29 16:44:26 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+{
+printf("it is a passitive number");
+}
+else if(n<0)
+{
+printf("it is a negative number");
+}
+else
+{
+printf("it is zero");
+}
+}
